752c0430450202da228397fe36da756f21089d1d
danieljanderson <danieljanderson0616@gmail.com>
2020-07-13 18:04:41 -0400

    learned how to bind datat

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     ],
     otherState: 'some other value',
   };
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('was clicked!');
     // dont do this this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 },
       ],
@@ -28,7 +28,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -36,6 +38,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
         >
           My Hobbies: Racing
         </Person>
