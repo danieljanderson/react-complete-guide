f8329c36e789673d7e24181ad9e24bc7de1287c9
danieljanderson <danieljanderson0616@gmail.com>
2020-07-24 18:01:22 -0400

    moved everything to new folder

---
+++ section3/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+    otherState: 'some other value',
+  };
+  switchNameHandler = (newName) => {
+    //console.log('was clicked!');
+    // dont do this this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian!!')}
+        >
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        ></Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangeHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        ></Person>
+      </div>
+    );
+  }
+}
+
+export default App;
